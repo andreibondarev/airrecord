328c2c4fca89c9dbf9b1cf641cc7e36f25735fd0
Andrei Bondarev <andrei.bondarev13@gmail.com>
2023-03-22 10:41:01 -0500

    Remove binding.pry

--- lib/airtable/base.rb
+++ lib/airtable/base.rb
@@ -21,7 +21,6 @@ module Airtable
     # Options: limit = 100, offset = "as345g", sort = ["Name", "asc"]
     # records(:sort => ["Name", :desc], :limit => 50, :offset => "as345g")
     def records(options={})
-      binding.pry
       options["sortField"], options["sortDirection"] = options.delete(:sort) if options[:sort]
       results = self.class.get(worksheet_url, query: options).parsed_response
       check_and_raise_error(results)
